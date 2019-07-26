780d38e9f47f702c27da72bb54e4942bf990754a
sockbot <github@sockbot.com>
2019-07-26 02:55:55 +0000

    added helper functions and tests

--- social.js
+++ social.js
@@ -45,6 +45,18 @@ let biggestFollower = function() {
   return name;
 }
 
+let mostPopular = function() {
+  let biggestFollowedCount = 0;
+  let name = '';
+  for (let uid in data) {
+    if (getNumFollowed(uid) > biggestFollowedCount) {
+      biggestFollowedCount = getNumFollowed(uid);
+      name = data[uid].name;
+    }
+  }
+  return name;
+}
+
 let getNumFollowing = function(uid) {
   return data[uid].follows.length;
 }
@@ -61,5 +73,46 @@ let getNumFollowed = function(uid) {
   return followedCount;
 }
 
+let doesFollow = function(uid1, uid2) {
+  // returns true if uid1 follows uid2
+  let uid1Followers = data[uid1].follows;
+  for (element of uid1Followers) {
+    if (element === uid2) {
+      return true;
+    }
+  } 
+  return false;
+}
+
+let getNameFromUid = function(uid) {
+  return data[uid].name;
+}
+
+let getFollowedList = function(uid) {
+  let followedList = [];
+  for (name of data[uid].follows) {
+    followedList.push(name);
+  }
+  return followedList;
+}
+
+/// fix this
+let getFollowerList = function(uid) {
+  let followerList = [];
+  // console.log(data['f01'].follows);
+  for (let person in data) {
+    for (let followed in data[person].follows) {
+      if (uid === followed) {
+        followerList.push(person);
+      }
+    }
+  }
+}
+
 console.log('Biggest follower is', biggestFollower());
 console.log('Followed count for f06 is', getNumFollowed('f06'));
+console.log('Most popular is', mostPopular());
+console.log('f01 follows f02', doesFollow('f01', 'f02'));
+console.log('f01 follows f05', doesFollow('f01', 'f05'));
+console.log('Followed list of f01:', getFollowedList('f01'))
+console.log('Follower list of f01:', getFollowerList('f01'))
